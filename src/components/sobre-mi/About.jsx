4f95df2b242025bfc9a089ca5e46643fc7170a05
Ricardo Meneses <ricardo@raccoondev.studio>
2020-03-29 17:24:27 -0600

    download CV added

--- src/components/sobre-mi/About.jsx
+++ src/components/sobre-mi/About.jsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react'
 import me from '../../assets/me.jpg'
+import CV from '../../assets/CV.pdf'
 import linkedin from '../../assets/linkedin.svg'
 import github from '../../assets/github.svg'
+import iconReact from '../../assets/react.svg'
+import iconVue from '../../assets/vuejs.svg'
+import iconNode from '../../assets/nodejs.svg'
+import iconCss from '../../assets/css3.svg'
+import iconHtml from '../../assets/html5.svg'
 import './about.css'
 
 const About = ({setHome}) => {
@@ -35,14 +41,27 @@ const About = ({setHome}) => {
               </div>
               <div className="about__card-below">
                 <div className="row">
-                  <div className="col-12 col-md-6">
+                  <div className="col-12 col-md-6 text-center text-md-left">
                     <h2>Sobre Mí</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ratione quam ducimus numquam aut quia quod, labore omnis nam dolores beatae ab exercitationem libero animi maiores pariatur harum. Fuga, aperiam?</p>
+                    <p>
+                      Soy una hombre en busca de mejorar mis habilidades, 
+                      cuento con dos años de experiencia en desarrollo web
+                      utilizando frameworks como Vue.js, Node.js, React.
+                    </p>
+                    <div className="about__tecnologies text-center">
+                      <img src={iconReact} alt="react" width="35" />
+                      <img src={iconVue} alt="vue" width="40" />
+                      <img src={iconNode} alt="node" width="40" />
+                      <img src={iconCss} alt="css" width="40" />
+                      <img src={iconHtml} alt="html" width="40" />
+                      <a href={CV} download="RicardoMenesesMorales.pdf" className="btn btn-success ml-0 ml-lg-5">Descargar CV</a>
+                    </div>
                   </div>
-                  <div className="col-12 col-md-6">
-                    <p><b>Edad:</b> 27 años</p>
-                    <p><b>Correo:</b> ricardomeneses0503@gmail.com</p>
-                    <p><b>Tel:</b> 5577197085</p>
+                  <div className="col-12 col-md-6 mt-3 mt-md-0">
+                    <p className="m-1 m-md-3"><b>Edad:</b> 27 años</p>
+                    <p className="m-1 m-md-3"><b>Correo:</b> ricardomeneses0503@gmail.com</p>
+                    <p className="m-1 m-md-3"><b>Tel:</b> 5577197085</p>
+                    <p className="m-1 m-md-3"><b>Domicilio:</b> Atizapan Zaragoza, Estado de México.</p>
                   </div>
                 </div>
               </div>
